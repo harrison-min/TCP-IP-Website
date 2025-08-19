cfa1de0ac0e5890c10a407d0277e73dc5761f5a2
harrison-min <harrison0min@gmail.com>
2025-08-19 14:54:22 -0700

    Aditional server.c work

--- src/TCP server/server.c
+++ src/TCP server/server.c
@@ -11,7 +11,7 @@ struct addrinfo * initializeAddrInfo ();
 SOCKET createSocket(struct addrinfo* addr);
 
 void serverShutdown();
-void closeSocket();
+void closeSocket(SOCKET socketToClose);
 
 
 
@@ -24,11 +24,15 @@ int main (int argc, char *argv []) {
 }
 
 SOCKET serverStartup() {
-	if (initializeWinsock ()) {
+	if (initializeWinsock () == false) {
 		return INVALID_SOCKET;
 	}
 	
 	struct addrinfo * addr = initializeAddrInfo ();
+	if (addr == NULL) {
+		return INVALID_SOCKET;
+	}
+	
 	SOCKET serverSocket = createSocket(addr);
 	
 	freeaddrinfo(addr);
@@ -51,24 +55,45 @@ bool initializeWinsock () {
 
 
 struct addrinfo * initializeAddrInfo () {
-	struct addrinfo hints, *result;
+	struct addrinfo hints, *result = NULL;
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(NULL, "8080", &hints, &result);
+	int status = getaddrinfo(NULL, "8080", &hints, &result);
+	if(status != 0) {
+		fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(status));
+	}
+	
 	
 	return result;
 }
 
 SOCKET createSocket(struct addrinfo* addr) {
+	SOCKET newSocket = socket (addr->ai_family, addr->ai_socktype, addr->ai_protocol);
 	
+	if (newSocket == INVALID_SOCKET) {
+		fprintf(stderr, "Socket creation failed %d\n", WSAGetLastError());
+		return INVALID_SOCKET;
+	}
+	
+	
+	
+	return newSocket;
 }
 
 
 void serverShutdown () {
-	WSACleanup();
 	closeSocket();
+	WSACleanup();
+}
+
+void closeSocket(SOCKET socketToClose) {
+	int result = closesocket(socketToClose);
+	
+	if (result == SOCKET_ERROR) {
+		fprintf(stderr, "closeSocket Failed with error %d\n", WSAGetLastError());
+	}
 }
